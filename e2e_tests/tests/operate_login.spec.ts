5095bf979d4217721d3ef1f17aabff542b2e139d
Jesse Simpson <jesse.simpson@camunda.com>
2023-05-18 12:03:59 -0400

    feat(ci): added release notes and github workflows to stable branch (#159)

---
+++ e2e_tests/tests/operate_login.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+const playwright = require('playwright');
+
+test('test', async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto('http://localhost:8081/');
+  await page.getByRole('heading', { name: 'Log in' }).waitFor();
+  await page.getByLabel('Username or email').click();
+  await page.getByLabel('Username or email').fill('demo');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('demo');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Processes' }).click();
+});
---
+++ release-notes-fetcher/fetch.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"context"
+	"github.com/google/go-github/v51/github"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/oauth2"
+	"io"
+	"os"
+	"regexp"
+	"text/template"
+)
+
+const RepoOwner = "camunda"
+const CloudRepoOwner = "camunda-cloud"
+const MainRepoName = "camunda-platform"
+const ZeebeRepoName = "zeebe"
+const OperateRepoName = "operate"
+const TasklistRepoName = "tasklist"
+const IdentityRepoName = "identity"
+const ReleaseNotesTemplateFileName = "release-notes-template.txt"
+
+type CamundaPlatformRelease struct {
+	ZeebeReleaseNotes    string
+	OperateReleaseNotes  string
+	TasklistReleaseNotes string
+	IdentityReleaseNotes string
+}
+
+func GetChangelogReleaseContents(ctx context.Context,
+	repoName string,
+	changelogFileName string,
+	repoService *github.RepositoriesService,
+	githubRef string) string {
+	opts := github.RepositoryContentGetOptions{
+		Ref: githubRef,
+	}
+	operateChangeLogReader, response, err := repoService.DownloadContents(ctx,
+		RepoOwner,
+		repoName,
+		changelogFileName,
+		&opts)
+	if err != nil || response.StatusCode != 200 {
+		log.Error().Stack().Err(err).Msg("an error has occurred")
+		os.Exit(1)
+	}
+
+	bytes, err := io.ReadAll(operateChangeLogReader)
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("an error has occurred")
+		os.Exit(1)
+	}
+	// operateChangeLogString := string(bytes)
+	latestReleaseRegex, err := regexp.Compile(`(?s)(?m)# .*?(?:^# )`)
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("an error has occurred")
+		os.Exit(1)
+	}
+	mostRecentChangeLog := latestReleaseRegex.Find(bytes)
+	var firstNewlineIndex int
+	for i, s := range mostRecentChangeLog {
+		if s == '\n' {
+			firstNewlineIndex = i
+			break
+		}
+	}
+	mostRecentChangeLogString := string(mostRecentChangeLog[firstNewlineIndex : len(mostRecentChangeLog)-2])
+	return mostRecentChangeLogString
+}
+
+func GetLatestReleaseContents(ctx context.Context,
+	orgName string,
+	repoName string,
+	repoService *github.RepositoriesService,
+	githubRef string) string {
+
+	githubRelease, response, err := repoService.GetReleaseByTag(ctx, orgName, repoName, githubRef)
+	if err != nil || response.StatusCode != 200 {
+		log.Error().Stack().Err(err).Msg("An error has occurred")
+		os.Exit(1)
+	}
+	return *githubRelease.Body
+}
+
+func main() {
+	var temp = template.Must(template.ParseFiles(ReleaseNotesTemplateFileName))
+
+	camundaTokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_CAMUNDA_ACCESS_TOKEN")},
+	)
+	githubRef := os.Getenv("GITHUB_REF_NAME")
+	ctx := context.TODO()
+	camundaOAuthClient := oauth2.NewClient(ctx, camundaTokenSource)
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	camundaGithubClient := github.NewClient(camundaOAuthClient)
+
+	camundaRepoService := camundaGithubClient.Repositories
+
+	log.Debug().Msg("Github ref = " + githubRef)
+
+	zeebeReleaseNotes := GetLatestReleaseContents(
+		ctx,
+		RepoOwner,
+		ZeebeRepoName,
+		camundaRepoService,
+		githubRef,
+	)
+
+	operateReleaseNotesContents := GetChangelogReleaseContents(
+		ctx,
+		OperateRepoName,
+		"CHANGELOG.md",
+		camundaRepoService,
+		githubRef,
+	)
+
+	tasklistReleaseNotesContents := GetChangelogReleaseContents(
+		ctx,
+		TasklistRepoName,
+		"CHANGELOG.md",
+		camundaRepoService,
+		githubRef,
+	)
+
+	camundaCloudTokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_CAMUNDA_CLOUD_ACCESS_TOKEN")},
+	)
+	camundaCloudOAuthClient := oauth2.NewClient(ctx, camundaCloudTokenSource)
+	camundaCloudGithubClient := github.NewClient(camundaCloudOAuthClient)
+	camundaCloudRepoService := camundaCloudGithubClient.Repositories
+
+	identityReleaseNotesContents := GetLatestReleaseContents(
+		ctx,
+		CloudRepoOwner,
+		IdentityRepoName,
+		camundaCloudRepoService,
+		githubRef,
+	)
+
+	platformRelease := CamundaPlatformRelease{
+		ZeebeReleaseNotes:    zeebeReleaseNotes,
+		OperateReleaseNotes:  operateReleaseNotesContents,
+		TasklistReleaseNotes: tasklistReleaseNotesContents,
+		IdentityReleaseNotes: identityReleaseNotesContents,
+	}
+
+	err := temp.Execute(os.Stdout, platformRelease)
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("could not parse template file")
+		os.Exit(1)
+	}
+}

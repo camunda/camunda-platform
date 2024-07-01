3647c90aebfc9b6257f14c791b31cf5aa70c08b9
marcosgvieira <marcos.vieira@camunda.com>
2024-07-01 12:25:32 +0100

    fix: fixing release notes for 8.4

--- release-notes-fetcher/fetch.go
+++ release-notes-fetcher/fetch.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	"context"
-	"github.com/google/go-github/v54/github"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"golang.org/x/oauth2"
+	"fmt"
 	"io"
 	"os"
 	"regexp"
 	"text/template"
+
+	"github.com/google/go-github/v54/github"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/oauth2"
+	"github.com/Masterminds/semver/v3"
 )
 
 const RepoOwner = "camunda"
 const CloudRepoOwner = "camunda-cloud"
 const MainRepoName = "camunda-platform"
-const ZeebeRepoName = "zeebe"
-const OperateRepoName = "operate"
+const ZeebeRepoName = "camunda"
 const TasklistRepoName = "tasklist"
 const IdentityRepoName = "identity"
 const ReleaseNotesTemplateFileName = "release-notes-template.txt"
 
 type CamundaPlatformRelease struct {
+	Overview             string
 	ZeebeReleaseNotes    string
 	OperateReleaseNotes  string
 	TasklistReleaseNotes string
 	IdentityReleaseNotes string
+}
+
+type camundaAppVersions struct {
+	Zeebe    string
+	Operate  string
+	Tasklist string
+	Identity string
 }
 
 func GetChangelogReleaseContents(ctx context.Context,
@@ -83,13 +93,44 @@ func GetLatestReleaseContents(ctx context.Context,
 	return *githubRelease.Body
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func releaseOverview(cav camundaAppVersions) string {
+	releaseOverview := `
+Camunda application in this release generation:
+- Identity: %s
+- Operate: %s
+- Tasklist: %s
+- Zeebe: %s
+`
+	return fmt.Sprintf(releaseOverview,
+		cav.Identity,
+		cav.Operate,
+		cav.Tasklist,
+		cav.Zeebe,
+	)
+}
+
 func main() {
 	var temp = template.Must(template.ParseFiles(ReleaseNotesTemplateFileName))
 
 	camundaTokenSource := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: os.Getenv("GITHUB_CAMUNDA_ACCESS_TOKEN")},
 	)
-	githubRef := os.Getenv("GITHUB_REF_NAME")
+
+	camundaReleaseVersion := getEnv("CAMUNDA_RELEASE_NAME", os.Getenv("GITHUB_REF_NAME"))
+	camundaAppVersions := camundaAppVersions{
+		Identity: getEnv("IDENTITY_GITREF", camundaReleaseVersion),
+		Operate:  getEnv("OPERATE_GITREF", camundaReleaseVersion),
+		Tasklist: getEnv("TASKLIST_GITREF", camundaReleaseVersion),
+		Zeebe:    getEnv("ZEEBE_GITREF", camundaReleaseVersion),
+	}
+
 	ctx := context.TODO()
 	camundaOAuthClient := oauth2.NewClient(ctx, camundaTokenSource)
 
@@ -100,30 +141,57 @@ func main() {
 
 	camundaRepoService := camundaGithubClient.Repositories
 
-	log.Debug().Msg("Github ref = " + githubRef)
+	log.Debug().Msg("Camunda Github ref = " + camundaReleaseVersion)
+	log.Debug().Msg("Zeebe Github ref = " + camundaAppVersions.Zeebe)
+	log.Debug().Msg("Tasklist Github ref = " + camundaAppVersions.Tasklist)
+	log.Debug().Msg("Operate Github ref = " + camundaAppVersions.Operate)
+	log.Debug().Msg("Identity Github ref = " + camundaAppVersions.Identity)
 
 	zeebeReleaseNotes := GetLatestReleaseContents(
 		ctx,
 		RepoOwner,
 		ZeebeRepoName,
 		camundaRepoService,
-		githubRef,
+		camundaAppVersions.Zeebe,
 	)
+
+	operateMonoRepoVersion, operateMonoErr := semver.NewVersion("8.5.0")
+	if operateMonoErr != nil {
+		log.Error().Stack().Err(operateMonoErr).Msg("Error parsing 8.5.0 version:")
+		return
+	}
+
+	operateCurrentVersion, err1 := semver.NewVersion(camundaAppVersions.Operate)
+	if err1 != nil {
+		log.Error().Stack().Err(err1).Msg("Error parsing operate version:")
+		return
+	}
+
+	var OperateRepoTag = ""
+	var OperateRepoName = ""
+	if(operateCurrentVersion.LessThan(operateMonoRepoVersion)) {
+		OperateRepoName = "operate"
+		OperateRepoTag = camundaAppVersions.Operate
+	}else {
+		OperateRepoName = "camunda"
+		OperateRepoTag = "operate-" + camundaAppVersions.Operate
+	}
 
 	operateReleaseNotesContents := GetLatestReleaseContents(
 		ctx,
 		RepoOwner,
 		OperateRepoName,
 		camundaRepoService,
-		githubRef,
+		OperateRepoTag,
 	)
+
 
 	tasklistReleaseNotesContents := GetChangelogReleaseContents(
 		ctx,
 		TasklistRepoName,
 		"CHANGELOG.md",
 		camundaRepoService,
-		githubRef,
+		camundaAppVersions.Tasklist,
 	)
 
 	camundaCloudTokenSource := oauth2.StaticTokenSource(
@@ -138,10 +206,15 @@ func main() {
 		CloudRepoOwner,
 		IdentityRepoName,
 		camundaCloudRepoService,
-		githubRef,
+		camundaAppVersions.Identity,
 	)
 
+	// Remove the Zeebe version at the beginning of Zeebe release notes to avoid confusion.
+	zeebeRegex := regexp.MustCompile(`# Release 8\..+\n`)
+	zeebeReleaseNotes = zeebeRegex.ReplaceAllString(zeebeReleaseNotes, "")
+
 	platformRelease := CamundaPlatformRelease{
+		Overview:             releaseOverview(camundaAppVersions),
 		ZeebeReleaseNotes:    zeebeReleaseNotes,
 		OperateReleaseNotes:  operateReleaseNotesContents,
 		TasklistReleaseNotes: tasklistReleaseNotesContents,

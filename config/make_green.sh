ef8b90c374a1fb9ce8bfe231200e4c0ec691ea40
Yevhen Podlyzhnyi <yevhen.podluzhnyi@capgemini.com>
2024-09-05 16:50:30 +0300

    initial unified compose file

---
+++ config/make_green.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+apk add curl;
+
+curl -k --request PUT https://opensearch:9200/myauditlogindex/_settings --header "Content-Type: application/json" -u admin:${OPENSEARCH_INITIAL_ADMIN_PASSWORD} --data '{"index" : {"number_of_replicas" : 0}}'

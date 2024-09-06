50d70fac36fa322a79123c9554bf9ecce1ce8df9
Yevhen Podluzhnyi <kpiwelder@gmail.com>
2024-09-06 16:52:58 +0300

    removed ssl

--- config/make_green.sh
+++ config/make_green.sh
@@ -2,4 +2,4 @@
 
 apk add curl;
 
-curl -k --request PUT https://opensearch:9200/myauditlogindex/_settings --header "Content-Type: application/json" -u admin:${OPENSEARCH_INITIAL_ADMIN_PASSWORD} --data '{"index" : {"number_of_replicas" : 0}}'
+curl -k --request PUT http://opensearch:9200/myauditlogindex/_settings --header "Content-Type: application/json" -u admin:${OPENSEARCH_INITIAL_ADMIN_PASSWORD} --data '{"index" : {"number_of_replicas" : 0}}'

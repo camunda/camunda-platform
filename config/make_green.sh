a6e83e20f6fe5719ffe4ffbc0dd5bfada8096e36
Yevhen Podluzhnyi <yevhen.podluzhnyi-ext@camunda.com>
2024-09-09 17:24:19 +0300

    fix: core changes
    - added .env configs
    - removed sh file
    - unified docker-compose.yml and docker-compose-core.yml

--- config/make_green.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-apk add curl;
-
-curl -k --request PUT http://opensearch:9200/myauditlogindex/_settings --header "Content-Type: application/json" -u admin:${OPENSEARCH_INITIAL_ADMIN_PASSWORD} --data '{"index" : {"number_of_replicas" : 0}}'
